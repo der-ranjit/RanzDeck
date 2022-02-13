c08d597d3b7bf3f5c40ce6fc8d1cf2aa12557675
Ranjit Mevius <ranjit.mevius@outlook.com>
2022-02-13 17:36:51 +0100

    init setup / template

---
+++ RanzDeck/Cards/DrFatBot.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace RanzCards.Cards
+{
+    class DrFatBot : CustomCard
+    {
+        
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            UnityEngine.Debug.Log($"[{RanzDeck.ModInitials}][Card] {GetTitle()} has been setup.");
+            block.cdMultiplier = 0.5f;
+            statModifiers.health = 0.5f;
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+            UnityEngine.Debug.Log($"[{RanzDeck.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+            UnityEngine.Debug.Log($"[{RanzDeck.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+
+        }
+
+        protected override string GetTitle()
+        {
+            return "Dr. Fat Bot";
+        }
+
+        protected override string GetDescription()
+        {
+            return "I like turtles";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Health",
+                    amount = "-50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Block Cooldown",
+                    amount = "-50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+
+        public override string GetModName()
+        {
+            return RanzDeck.ModInitials;
+        }
+    }
+}
---
+++ RanzDeck/RanzDeck.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using UnboundLib.Cards;
+using HarmonyLib;
+using RanzCards.Cards;
+
+namespace RanzCards
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class RanzDeck : BaseUnityPlugin
+    {
+        // needed for BepinEx stuff (exposes the GameObject)
+        public static RanzDeck instance { get; private set; }
+
+        public const string ModInitials = "OOF";
+        public const string Version = "1.0.0";
+        private const string ModName = "RanzDeck";
+        private const string ModId = "ranz.rounds.ranzdeck";
+
+        void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<DrFatBot>();
+        }
+    }
+}
